d0671743bc51795afe381beb54b25121d4d5607b
Arun <arun@example.com>
2026-07-10 15:28:15 +0530

    add modue

---
+++ modul.py
@@ -0,0 +1,12 @@
+import pyjokes
+
+# print("Printing a random joke from pyjokes library:")
+
+# This print a random joke
+joke = pyjokes.get_joke()
+print(joke)
+
+""" 
+So thanks that was my program that prints a random joke from the pyjokes library. 
+
+"""
