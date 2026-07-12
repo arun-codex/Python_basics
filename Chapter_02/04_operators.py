ffb3ab80bbe1591a1b25465d64bd67f2507284ed
arun.codex <arun.codex@gmail.com>
2026-07-13 00:23:22 +0530

    feat: add arithmetic operators demonstration script

---
+++ Chapter_02/04_operators.py
@@ -0,0 +1,12 @@
+# Arithmetic operators
+
+a = 18 
+
+b = 6 
+
+print("the sum of a and b is ", a+b)
+print("the difference of a and b is ", a-b)
+print("the product of a and b is ", a*b)
+print("the division of a and b is ", a/b)
+print("the modulo of a and b is ", a%b)
+print("the exponent of a and b is ", a**b)
