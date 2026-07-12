7f6f3e8232e866fe88959682fb5f8eddb199fadb
arun.codex <arun.codex@gmail.com>
2026-07-13 01:40:17 +0530

    feat: add solutions for chapter 2 problem set exercises

---
+++ chapter_02 PS/problem_04.py
@@ -0,0 +1,8 @@
+# Use comparison operator to find out whether ‘aʼ given variable is greater than ‘bʼ or not.
+# Take a = 34 and b = 80
+
+a = 34
+b = 80 
+
+print("a is greater than b : ", a>b)
+
---
+++ chapter_02 PS/problem_06.py
@@ -0,0 +1,13 @@
+
+# Write a python program to calculate the square of a number entered by the user
+
+a = int(input("Enter the a number whose square you want : "))
+
+square = a * a 
+
+print("The square of a is : ", square)
+
+#another way to write this 
+print(f"The square of {a} is {square}")
+
+
