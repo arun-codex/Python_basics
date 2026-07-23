1e15d3836bc486fe9380b7d72e79f3abdabe8ea1
Arun Kumar <arun.cyberx@gmail.com>
2026-07-23 16:29:11 +0530

    feat: add function practice scripts for calculating averages and printing greetings

---
+++ Chapter_08/01_functions.py
@@ -0,0 +1,33 @@
+# a = int(input("Enter a number: "))
+
+# b = int(input("Enter a number: "))
+
+# c = int(input("Enter a number: "))
+
+# average = (a + b + c)/3
+
+# a = int(input("Enter a number: "))
+
+# b = int(input("Enter a number: "))
+
+# c = int(input("Enter a number: "))
+
+# average = (a + b + c)/3
+
+# print("The average of", a, ",", b, ",", c, "is", age)
+
+
+# function definition
+def avg():
+    a = int(input("Enter a number: "))
+
+    b = int(input("Enter a number: "))
+
+    c = int(input("Enter a number: "))
+
+    average = (a + b + c)/3
+
+    print("The average of", a, ",", b, ",", c, "is", average)
+
+avg()    # function call  
+ 
