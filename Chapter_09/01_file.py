947063f7a1bb1b9cff703106b71260c91be0b0a7
arun.codex <arun.codex@gmail.com>
2026-07-27 01:38:55 +0530

    feat: add Python file I/O examples covering reading, writing, and appending operations

---
+++ Chapter_09/01_file.py
@@ -0,0 +1,13 @@
+'''
+a = "a very long string with emails"
+emails = []
+
+3 seconds
+'''
+
+
+f = open("Chapter_09/file.txt")
+data = f.read()
+print(data)
+f.close()
+
---
+++ Chapter_09/02_file_write.py
@@ -0,0 +1,8 @@
+st = "Hey Arun you are a good man." 
+
+
+f = open("Chapter_09/myfile.txt", "w")
+
+f.write(st)
+
+f.close()
