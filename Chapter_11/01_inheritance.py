795208b3540e5bb458ca60f953ef846b75690e57
arun.codex <arun.codex@gmail.com>
2026-08-01 00:02:35 +0530

    feat: implement single, multiple, and multilevel inheritance examples in Chapter 11

--- Chapter_11/01_inheritance.py
+++ Chapter_11/01_inheritance.py
@@ -1,12 +1,21 @@
 class Employee:
     company = "ITC"
+
     def show(self):
         print(f"The name is {self.name} and the salary is {self.salary}")
 
-class Programmer:
+
+# class Programmer:
+#     company = "ITC Infotech"
+#     def show(self):
+#         print(f"The name is {self.name} and the salary is {self.salary}")
+
+#     def showLanguage(self):
+#         print(f"The name is {self.name} and he is good with {self.language} language")
+
+
+class Programmer(Employee):
     company = "ITC Infotech"
-    def show(self):
-        print(f"The name is {self.name} and the salary is {self.salary}")
 
     def showLanguage(self):
         print(f"The name is {self.name} and he is good with {self.language} language")
---
+++ Chapter_11/02_multiple_inheritance.py
@@ -0,0 +1,24 @@
+class Employee:
+    company = "ITC"
+    name =  "Default name"
+    def show(self):
+        print(f"The name is {self.name} and the company is {self.company}")
+
+class coder:
+    language = "Python"
+    def printlanguage(self):
+        print(f"Out of all the languages here is your language: {self.language}")
+
+class Programmer(Employee, coder):
+    company = "ITC Infotech"
+
+    def showLanguage(self):
+        print(f"The name is {self.company} and he is good with {self.language} language")
+
+
+a = Employee()
+b = Programmer()
+
+b.show()
+b.showLanguage()
+b.printlanguage()
