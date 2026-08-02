67297d75137857f3faeb99e50fdf6d66a9a1e552
arun.codex <arun.codex@gmail.com>
2026-08-02 23:20:15 +0530

    feat: add 2D and 3D vector classes and comment out incomplete leap year function

--- Chapter_08 PS/practice/Function_02/06.py
+++ Chapter_08 PS/practice/Function_02/06.py
@@ -1,9 +1,7 @@
-# 6. Return whether a year is a leap year.
-# leap year is divide by 4 and 400 but not divide by 100
+# # 6. Return whether a year is a leap year.
+# # leap year is divide by 4 and 400 but not divide by 100
 
-def leap_year(year):
-    if year % 4 == 0:
-        if year % 400 == 0:
+# def leap_year(year):
+#     if year % 4 == 0:
+#         if year % 400 == 0:
             
-
-
