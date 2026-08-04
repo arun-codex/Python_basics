a0ca54ea2581a46d2b3fd6e582e0218221bab127
Arun Kumar <arun.cyberx@gmail.com>
2026-08-04 15:34:32 +0530

    feat: implement Vector class with addition and dot product support

---
+++ Chapter_11 PS/problem5.py
@@ -0,0 +1,16 @@
+class Vector:
+    def __init__(self,x,y,z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __add__(self, other):
+        result = Vector(self.x + other.x, self.y + other.y, self.z + other.z)
+        return result
+
+    def __mul__(self,other):
+        result = self.x * other.x + self.y * other.y + self.z * other.z
+        return result
+
+    def __str__(self):
+        return f"Vector({self.x}, {self.y}, {self.z})"
