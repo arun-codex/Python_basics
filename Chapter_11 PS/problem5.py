c6a4d28d8edbd3d6c37c28a40179a5129f921d59
Arun Kumar <arun.cyberx@gmail.com>
2026-08-04 20:07:53 +0530

    feat: add test cases for Vector addition and multiplication operations

--- Chapter_11 PS/problem5.py
+++ Chapter_11 PS/problem5.py
@@ -14,3 +14,16 @@ class Vector:
 
     def __str__(self):
         return f"Vector({self.x}, {self.y}, {self.z})"
+
+
+# Test the implementation 
+
+v1 = Vector(1,2,3)
+v2 = Vector(4,5,6)
+v3 = Vector(7,8,9)
+
+print(v1 + v2)
+print(v1 * v2)
+
+print(v1 + v3)
+print(v1 * v3)
