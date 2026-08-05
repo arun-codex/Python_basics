9ca12dec54db85d2ec43bbe20d987e4752fab1bb
Arun Kumar <arun.cyberx@gmail.com>
2026-08-05 20:48:48 +0530

    Add Chapter 12 examples

---
+++ Chapter_12/08_try_finally.py
@@ -0,0 +1,12 @@
+def main():
+    try:
+        a = int(input("Hey, Enter a Number: "))
+        print(a)
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        print("I am inside of finally.")
+
+main()
