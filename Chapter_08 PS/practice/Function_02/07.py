05e9e83e72a2a13843e8ddd8be1d6602ef2c1f43
Arun <arun@example.com>
2026-08-06 17:01:03 +0530

    feat: add function to calculate the area of a circle

---
+++ Chapter_08 PS/practice/Function_02/07.py
@@ -0,0 +1,10 @@
+# 7. Return the area of a circle.
+
+def area_of_circle(radius):
+    pi = 3.14
+    return f"Area of circle is {pi*(radius**2)}"
+
+radius = 5
+area = area_of_circle(radius)
+
+print(area)
