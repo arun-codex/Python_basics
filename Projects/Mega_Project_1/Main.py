7ad93e3e7dba14b9d26f653633720380d656f2b4
Arun Kumar <arun.cyberx@gmail.com>
2026-08-08 07:18:37 +0530

    feat: implement voice command functionality to open websites

--- Projects/Mega_Project_1/Main.py
+++ Projects/Mega_Project_1/Main.py
@@ -1,1 +1,43 @@
 import speech_recognition as sr
+import webbrowser
+import pyttsx3
+
+recognizer = sr.Recognizer()
+engine = pyttsx3.init()
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def processCommand(c):
+    if "open google" in c.lower():
+         webbrowser.open("https://google.com")
+    elif "open facebook" in c.lower():
+        webbrowser.open("https://facebook.com")
+
+
+if __name__ == "__main__":
+    speak("Initializing jarvis.....")
+    while True: 
+        # Listen for the wake word "Jarvis"
+        # obtain audio from the microphone
+        r = sr.Recognizer()
+        
+        print("recognizing...")
+        try:
+            with sr.Microphone() as source:
+                print("Listening...")
+                audio = r.listen(source) 
+            command = r.recognize_google(audio)
+            if(command.lower() == "jarvis"):
+                speak("Ya")
+                # listen for command
+                with sr.Microphone() as source:
+                    print("Jarvis Active...")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+
+                    processCommand(command)
+            
+        except Exception as e:
+            print("Error; {0}".format(e))
